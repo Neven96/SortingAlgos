6ce0e6922f653d75a0e4dcc6605b73d01ba92bcd
Even <even.holmen@hotmail.no>
2025-02-17 19:51:26 +0100

    Added more comments and some name changes

--- Python/tester.py
+++ Python/tester.py
@@ -8,79 +8,150 @@ import merge
 import quick
 
 def main():
-    filename, algo, print_sorted_array = input_checker()
+    '''
+    Runs the test file, and prints out the finished products
+    '''
+
+    file_path, algo, print_sorted_list = input_checker()
     
-    array = read_file(filename)
+    unsorted_list = read_file(file_path)
     
-    sorted_array, compares, swaps, time_used = sorter(array, algo)
+    sorted_list, compares, swaps, time_used = sorter(list(unsorted_list), algo)
     
     # If for some reason something happens to the algorithms
-    if sorted_array != sorted(array):
+    if sorted_list != sorted(unsorted_list):
         print("LISTS NOT SORTED")
         
-    print('Sorted array using: ' + algo.__name__ + ' sort')
-    print('Using ' + str(compares) + ' compares and ' + str(swaps) + ' swaps')
+    print(f'Sorted list using: {algo.__name__} sort')
+    print(f'Using {str(compares)} compares and {str(swaps)} swaps')
     
-    time_symbol = 'ns'
-    
-    if time_used > 1000000000:
-        time_used = time_used/1000000000.0
-        time_symbol = 's'
-    elif time_used > 1000000:
-        time_used = time_used/1000000.0
-        time_symbol = 'ms'
+    # Time handling
+    time_used, time_symbol = time_handling(time_used)
         
-    print('Time used: ' + '{:.4f}'.format(time_used) + ' ' + time_symbol)
+    print(f'Time used: {time_used:.4f} {time_symbol}')
 
-    if print_sorted_array:
-        print('\nSorted Array:')
+    # Prints out the sorted list if you asked for it
+    if print_sorted_list:
+        print('\nSorted list:')
         print('----------------------------------------------------------------------')
-        print(sorted_array)
+        print(sorted_list)
 
-def sorter(array, algo):
-    start_time = time.process_time_ns()
-    sorted_array, compares, swaps = algo.sort(array)
-    time_used = time.process_time_ns() - start_time
-    
-    return sorted_array, compares, swaps, time_used
+def sorter(unsorted_list, algo):
+    '''
+    Handles the sorting of the list with the correct algorithm
+    Also handles the time keeping for each algorithm
 
-def read_file(filename):
-    array = []
+    Parameters
+    -----------
+    list : list
+        The list that shall be sorted
+    algo : module
+        The sorting algorithm to be used
+
+    Returns
+    -----------
+    sorted_list : list
+        The sorted list
+    compares : int
+        The amount of compares the algorithm used
+        Every time a while loop or if statement is called
+    swaps : int
+        Every time one item in the list is swaped to another place
+    time_used : int
+        The time used to run the algorithm
+        It is in nano seconds
+    '''
+    start_time = time.perf_counter_ns()
+    sorted_list, compares, swaps = algo.sort(unsorted_list)
+    time_used = time.perf_counter_ns() - start_time
+
+    return sorted_list, compares, swaps, time_used
+
+def read_file(file_path):
+    '''
+    Reads the file that contains an unsorted list
+
+    Parameters
+    -----------
+    file_path : str
+        The path to the file
+
+    Returns
+    -----------
+    unsorted_list : list
+        The unsorted list
+    '''
+
+    unsorted_list = []
     
     try:
-        with open(filename, 'r') as file:
-            array = [int(x) for x in file.readlines()]
+        with open(file_path, 'r') as file:
+            unsorted_list = [int(x) for x in file.readlines()]
     except FileNotFoundError:
         print('File not found!')
         sys.exit(1)
         
-    return array
+    return unsorted_list
     
 def input_checker():
+    '''
+    A function to handle all the inputs needed to sort with correct algorithm
+
+    Returns
+    -----------
+    file_path : string
+        The full path for the file that contains the unsorted list
+    algo : module
+        The module which contains the sorting algorithm
+    print_sorted_list : bool
+        If you want to print the sorted list or not
+    '''
+
+    # Gets the file path for all the inputs
     test_files_path = os.path.join('..', 'inputs')
     filenames = os.listdir(test_files_path)
     
+    # A list of all the modules for the sorting algorithms and a list of their names
     sorting_algos = [bubble, insertion, merge, quick]
     sorting_algo_names = [names.__name__ for names in sorting_algos]
     
-    filename = input_validator('Test File', filenames)
-    filename = os.path.join(test_files_path, filename)
+    # Gets the file path for the chosen file
+    filename = input_handler('Test File', filenames)
+    file_path = os.path.join(test_files_path, filename)
     
-    algo_name = input_validator('Sorting Algorithm', sorting_algo_names)
+    # Takes the name for the algo and gets the correct module
+    algo_name = input_handler('Sorting Algorithm', sorting_algo_names)
     algo = sorting_algos[sorting_algo_names.index(algo_name)]
+
+    print_sorted_list = input_handler('If You Want To Print The Sorted list', ['yes', 'no'])
+    print_sorted_list = True if print_sorted_list == 'yes' else False
     
-    print_sorted_array = input_validator('If You Want To Print The Sorted Array', ['yes', 'no'])
-    print_sorted_array = True if print_sorted_array == 'yes' else False
+    return file_path, algo, print_sorted_list
     
-    return filename, algo, print_sorted_array
-    
-def input_validator(description, array):
+def input_handler(description, input_list):
+    '''
+    Creates the prints and takes in user input for the choices of files,
+    algorithms and if you want the sorted list printed
+
+    Parameters
+    -----------
+    description : str
+        The string that describes for the user what they are choosing
+    input_list : list
+        The list of choices to be made by the user
+
+    Returns
+    -----------
+    chosen_name : str
+        The chosen list item from the input_list
+    '''
+
     while True:
-        print('Please Choose ' + description + ':')
+        print(f'Please Choose {description}:')
         print('----------------------------------------------------------------------')
         
-        for i in range(len(array)):
-            print(str(i+1) + ': ' + array[i])
+        for i in range(len(input_list)):
+            print(str(i+1) + ': ' + input_list[i])
         print('x: exit')
         
         choice = input('Choice: ')
@@ -97,24 +168,55 @@ def input_validator(description, array):
                 sys.exit(0)
             
             print('----------------------------------------------------------------------')
-            print('"' + choice + '" Is Not A Choice')
+            print(f'"{choice}" Is Not A Choice')
             print('----------------------------------------------------------------------\n')
 
             continue
-        if choice < 1 or choice > len(array):
+        if choice < 1 or choice > len(input_list):
             
             print('----------------------------------------------------------------------')
-            print('Please Choose A choice Between 1 And ' + str(len(array)))
+            print(
+                f'Please Choose A number Between 1 And {str(len(input_list))}')
             print('----------------------------------------------------------------------\n')
             
             continue
         break
         
-    name = array[choice-1]
+    chosen_name = input_list[choice-1]
     
-    print(description + ' Chosen: ' + name + '\n')
+    print(f'{description} Chosen: {chosen_name}\n')
     
-    return name
+    return chosen_name
     
+def time_handling(time_used):
+    '''
+    Handles converting of time to appropriate formats
+
+    Parameters
+    -----------
+    time_used : int
+        The time the program used in nanoseconds
+
+    Returns
+    -----------
+    time_used : int/float
+        The time used in either int nanoseconds
+        or converted to float milliseconds or seconds
+    time_symbol : str
+        The format/symbol, either ns for nanoseconds,
+        ms for milliseconds or s for seconds
+    '''
+
+    time_symbol = 'ns'
+
+    if time_used > 1000000000:
+        time_used = time_used/1000000000.0
+        time_symbol = 's'
+    elif time_used > 1000000:
+        time_used = time_used/1000000.0
+        time_symbol = 'ms'
+    
+    return time_used, time_symbol
+
 if __name__ == '__main__':
     main()
